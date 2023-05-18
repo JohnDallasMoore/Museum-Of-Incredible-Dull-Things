f9f2c42c82a9b8e2f24b4f5188d2520f962ebb28
Dallas Moore <moore.johndallas@gmail.com>
2023-05-17 22:20:25 -0500

    second attempt at completing challenge

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 function removeSmallest(numbers) {
-    let smallestIndex = 0;
-  
-    for (let i = 1; i < numbers.length; i++) {
-      if (numbers[i] < numbers[smallestIndex]) {
-        smallestIndex = i;
+    if (numbers.length === 0) {
+        return [];
       }
-    }
+    
+      const smallest = Math.min(...numbers);
+      const smallestIndex = numbers.indexOf(smallest);
+    
+      return [...numbers.slice(0, smallestIndex), ...numbers.slice(smallestIndex + 1)];
+
+//Old version
+
+    // let smallestIndex = 0;
   
-    const result = numbers.filter((_, index) => index !== smallestIndex);
+    // for (let i = 1; i < numbers.length; i++) {
+    //   if (numbers[i] < numbers[smallestIndex]) {
+    //     smallestIndex = i;
+    //   }
+    // }
   
-    return console.log(result);;
+    // const result = numbers.filter((_, index) => index !== smallestIndex);
+  
+    // return console.log(result);;
   }
   
-  removeSmallest([5,3,2,1,4]);
+  const result = removeSmallest([2,2,1,2,1]);
+  console.log(result);
